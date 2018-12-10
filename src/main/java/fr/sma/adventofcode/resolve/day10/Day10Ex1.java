c710b279704f9e26653749e505cee56e9ef51fd7
SMARION1 <steve.marion-ext@elis.com>
2018-12-10 18:55:01 +0100

    advance on day 10

--- src/main/java/fr/sma/adventofcode/resolve/day10/Day10Ex1.java
+++ src/main/java/fr/sma/adventofcode/resolve/day10/Day10Ex1.java
@@ -1,11 +1,17 @@
 package fr.sma.adventofcode.resolve.day10;
 
+import com.google.common.math.PairedStats;
+import com.google.common.math.PairedStatsAccumulator;
 import fr.sma.adventofcode.resolve.DataFetcher;
 import fr.sma.adventofcode.resolve.ExSolution;
+import java.util.Comparator;
+import java.util.Map;
+import one.util.streamex.IntStreamEx;
 import one.util.streamex.StreamEx;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.spel.ast.StringLiteral;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -28,10 +34,10 @@ public class Day10Ex1 implements ExSolution {
 		
 		String values = dataFetcher.fetch(10).trim();
 		
-		List<StarCluster> starList = StreamEx.split(values, "\n")
+		List<Star> starList = StreamEx.split(values, "\n")
 				.map(LINE_PATTERN::matcher)
 				.filter(Matcher::matches)
-				.map(m -> new StarCluster(
+				.map(m -> new Star(
 						Integer.parseInt(m.group(1)),
 						Integer.parseInt(m.group(2)),
 						Integer.parseInt(m.group(3)),
@@ -39,81 +45,76 @@ public class Day10Ex1 implements ExSolution {
 				))
 				.collect(Collectors.toList());
 		
-		StarCluster sc0 = starList.stream()
-				.reduce(StarCluster::merge).get();
-		
-		System.out.println("starcluster at t=0 : " + sc0);
+		IntStreamEx.range(10805, 10820, 1)
+				.mapToEntry(i -> i, i -> calculateStarArea(i, starList))
+				.forEach(System.out::println);
 	}
 	
-	private static class StarCluster {
-		final float x;
-		final float y;
-		final float dx;
-		final float dy;
-		final float weight;
-		final float size;
+	public double calculateStarArea(int t, List<Star> starList) {
+		PairedStatsAccumulator stat = new PairedStatsAccumulator();
+		starList.forEach(s -> stat.add(s.getX(t), s.getY(t)));
+		return (stat.xStats().max() - stat.xStats().min()) * (stat.yStats().max() - stat.yStats().min());
+	}
+	
+	public String printSky(int t, List<Star> starList) {
+		StreamEx.of(starList)
+				.sorted(Comparator.<Star, Integer>comparing(s -> s.getX(t)).thenComparing(s -> s.getY(t)))
+				.collapse((s1, s2) -> s1.getX(t) == s2.getX(t), )
+	}
+	
+	
+	private static class Star {
+		final int x0;
+		final int y0;
+		final int dx;
+		final int dy;
 		
-		@Override
-		public String toString() {
-			return "StarCluster{" +
-					"x=" + x +
-					", y=" + y +
-					", dx=" + dx +
-					", dy=" + dy +
-					", weight=" + weight +
-					", size=" + size +
-					'}';
-		}
 		
-		public StarCluster(float x, float y, float dx, float dy) {
-			this(x, y, dx, dy, 1, 1);
-		}
 		
-		private StarCluster(float x, float y, float dx, float dy, float weight, float size) {
-			this.x = x;
-			this.y = y;
+		private Star(int x0, int y0, int dx, int dy) {
+			this.x0 = x0;
+			this.y0 = y0;
 			this.dx = dx;
 			this.dy = dy;
-			this.weight = weight;
-			this.size = size;
 		}
 		
-		public static StarCluster merge(StarCluster s1, StarCluster s2) {
-			float newX = (s1.x*s1.weight + s2.x*s2.weight)/(s1.weight + s2.weight);
-			float newY = (s1.y*s1.weight + s2.y*s2.weight)/(s1.weight + s2.weight);
+		public int getX(int t) {
+			return x0 + dx*t;
+		}
+		
+		
+		public int getY(int t) {
+			return y0 + dy*t;
+		}
+		
+		/*public static Star merge(Star s1, Star s2) {
+			float newX = (s1.x0 *s1.weight + s2.x0 *s2.weight)/(s1.weight + s2.weight);
+			float newY = (s1.y0 *s1.weight + s2.y0 *s2.weight)/(s1.weight + s2.weight);
 			
 			float newDx = (s1.dx*s1.weight + s2.dx*s2.weight)/(s1.weight + s2.weight);
 			float newDy = (s1.dy*s1.weight + s2.dy*s2.weight)/(s1.weight + s2.weight);
 			
 			float newWeight = s1.weight + s2.weight;
 			
-			float newSize = (float) Math.hypot(Math.abs(s2.x - s1.x), Math.abs(s2.y - s1.y)) + s1.size/2 + s2.size/2;
+			float newSize = (float) Math.hypot(Math.abs(s2.x0 - s1.x0), Math.abs(s2.y0 - s1.y0)) + s1.size/2 + s2.size/2;
 			
-			return new StarCluster(newX, newY, newDx, newDy, newWeight, newSize);
+			return new Star(newX, newY, newDx, newDy, newWeight, newSize);
+		}*/
+		
+		public int getX0() {
+			return x0;
 		}
 		
-		public float getX() {
-			return x;
+		public int getY0() {
+			return y0;
 		}
 		
-		public float getY() {
-			return y;
-		}
-		
-		public float getDx() {
+		public int getDx() {
 			return dx;
 		}
 		
-		public float getDy() {
+		public int getDy() {
 			return dy;
-		}
-		
-		public float getWeight() {
-			return weight;
-		}
-		
-		public float getSize() {
-			return size;
 		}
 	}
 }
